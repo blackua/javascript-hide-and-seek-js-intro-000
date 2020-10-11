0e9224f5a088e81788c29fe77303fc2b710231b4
Norathat Jongjitnant <norathat@gmail.com>
2020-10-11 10:59:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelectorAll('div.target')[0]
+}
+
+function increaseRankBy(n){
+  const lis = document
+              .getElementById('app')
+              .querySelectorAll('ul.ranked-list li')
+
+  for (var i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+
+  return lis
+}
+
+function deepestChild(){
+  const main = document
+               .getElementById('grand-node')
+               .querySelectorAll('div')
+  for (var i = 0; i < main.length; i++) {
+    if (main[i].innerText != '') {
+      return main[i].innerText
+    }
+  }
+}
